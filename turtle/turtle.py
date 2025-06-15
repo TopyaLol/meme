8f6fe8e3bac3c1a374b5dbcea3f09b81b42895a1
TopyaLol <maryman0705@gmail.com>
2025-06-15 12:22:33 +0300

    Create turtle.py

---
+++ turtle/turtle.py
@@ -0,0 +1,19 @@
+import turtle
+import random
+
+turtle.shape("turtle")
+turtle.pencolor("red")
+
+#for i in range(4):
+ # turtle.forward(100)
+ # turtle.left(90)
+
+#for i in range(100):
+#  turtle.goto(random.randint(-300, 300), random.randint(-300, 300))
+
+turtle.dot(100)
+turtle.forward(200)
+turtle.circle(50)
+
+
+turtle.mainloop()
